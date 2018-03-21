3c4688854b0327ef20ea65ea62a278fc21102a00
廖鹏 <wb-lp299355@alibaba-inc.com>
2018-03-21 17:30:16 +0800

    修改header 样式

--- src/src/components/main/header.js
+++ src/src/components/main/header.js
@@ -9,24 +9,25 @@ import './style.css';
 export default () => {
   return (
     <div className="navbar">
-      <ul mode="horizontal" className="barstyle">
-        <li className="logostyle" key="logo">
-          <Icon className="Iconstyle" type="api" />
-          <a rel="noopener noreferrer" target="_blank">react · 学习平台</a>
-        </li>
-        <li key="全部">
-          <Link to="/all">全部</Link>
-        </li>
-        <li key="精华">
-          <Link to="/good">精华</Link>
-        </li>
-        <li key="分享">
-          <Link to="/share">分享</Link>
-        </li>
-        <li key="问答">
-          <Link to="/ask">问答</Link>
-        </li>
-      </ul>
+      <div className="navbar-inner">
+        <a className="brand" rel="noopener noreferrer" target="_blank">
+          <img src="http://o4j806krb.qnssl.com/public/images/cnodejs_light.svg" alt="logo"/>
+        </a>
+        <ul mode="horizontal" >
+          <li key="全部">
+            <Link to="/all">全部</Link>
+          </li>
+          <li key="精华">
+            <Link to="/good">精华</Link>
+          </li>
+          <li key="分享">
+            <Link to="/share">分享</Link>
+          </li>
+          <li key="问答">
+            <Link to="/ask">问答</Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
